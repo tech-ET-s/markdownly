068a22a0b9c45101aabdf47bf34634136716b802
Zach Butler <zach.standley93@gmail.com>
2019-08-28 17:10:01 -0700

    Created our MarkdownList component and a passing snapshot test

---
+++ src/components/list/MarkdownList.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MarkdownList from './MarkdownList';
+
+describe('MardownList component', () => {
+  it('render mardown list', () => {
+    const array = [{ title: 'calling' }, { title: 'bluf' }];
+    const wrapper = shallow(<MarkdownList markdowns={array} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
