6b50c679166aa650bd3cd43514165611509b3597
Zach Butler <zach.standley93@gmail.com>
2019-08-29 14:52:09 -0700

    refactored the actions

--- src/actions/markDownActions.test.js
+++ src/actions/markDownActions.test.js
@@ -1,12 +1,42 @@
-import { updateMarkdown, UPDATE_MARKDOWN } from './markdownActions';
+import { UPDATE_FORM_INPUT, updateFormInput, addMdFile, ADD_MD_FILE, clearFormInput, CLEAR_FORM_INPUT, updateMdBody, UPDATE_MD_BODY } from './addMdActions';
 
-describe('markdownAction test', () => {
-  it('it can return an markdown update action', () => {
-    const action = updateMarkdown('string');
+describe('addMdActions test', () => {
+  it('returns state of input', () => {
+    const action = updateFormInput('title');
 
     expect(action).toEqual({
-      type: UPDATE_MARKDOWN,
-      payload: 'string'
+      type: UPDATE_FORM_INPUT,
+      payload: 'title'
+    });
+  });
+
+  it('returns new object for md file', () => {
+    const action = addMdFile('title');
+
+    expect(action).toEqual({
+      type: ADD_MD_FILE,
+      payload: { title: 'title', body: '' }
+    });
+  });
+
+  it('returns empty string', () => {
+    const action = clearFormInput();
+
+    expect(action).toEqual({
+      type: CLEAR_FORM_INPUT,
+      payload: ''
+    });
+  });
+
+  it('returns an action with a body', () => {
+    const action = updateMdBody('body', 1);
+
+    expect(action).toEqual({
+      type: UPDATE_MD_BODY,
+      payload: {
+        body: 'body',
+        index: 1
+      }
     });
   });
 });
--- src/actions/markdownActions.js
+++ src/actions/markdownActions.js
@@ -1,5 +1,23 @@
-export const UPDATE_MARKDOWN = 'UPDATE_MARKDOWN';
-export const updateMarkdown = markdown => ({
-  type: UPDATE_MARKDOWN,
-  payload: markdown
+export const UPDATE_FORM_INPUT = 'UPDATE_FORM_INPUT';
+export const updateFormInput = title => ({
+  type: UPDATE_FORM_INPUT,
+  payload: title
+});
+
+export const ADD_MD_FILE = 'ADD_MD_FILE';
+export const addMdFile = (title) => ({
+  type: ADD_MD_FILE,
+  payload: { title, body: '' }
+});
+
+export const CLEAR_FORM_INPUT = 'CLEAR_FORM_INPUT';
+export const clearFormInput = () => ({
+  type: CLEAR_FORM_INPUT,
+  payload: ''
+});
+
+export const UPDATE_MD_BODY = 'UPDATE_MD_BODY';
+export const updateMdBody = (body, index) => ({
+  type: UPDATE_MD_BODY,
+  payload: { body, index }
 });
