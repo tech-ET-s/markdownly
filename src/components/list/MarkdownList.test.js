7ef1d7a1ae9389402bfdcbfffe1158064c7e7258
Zach Butler <zach.standley93@gmail.com>
2019-08-29 16:50:53 -0700

    MarkdownList test updated

--- src/components/list/MarkdownList.test.js
+++ src/components/list/MarkdownList.test.js
@@ -5,7 +5,9 @@ import MarkdownList from './MarkdownList';
 describe('MardownList component', () => {
   it('render mardown list', () => {
     const array = [{ title: 'calling' }, { title: 'bluf' }];
-    const wrapper = shallow(<MarkdownList markdowns={array} />);
+    const wrapper = shallow(<MarkdownList 
+      markdowns={array}
+      clickHandler={() => {}} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
