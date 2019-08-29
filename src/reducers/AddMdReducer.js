2d29329f6b542fe8c0f16db145da18c83f926183
Zach Butler <zach.standley93@gmail.com>
2019-08-29 11:15:40 -0700

    AddMdReducer adds md file obj to array in state

--- src/reducers/AddMdReducer.js
+++ src/reducers/AddMdReducer.js
@@ -1,9 +1,16 @@
-import { UPDATE_FORM_INPUT } from '../actions/addMdActions';
+import { UPDATE_FORM_INPUT, ADD_MD_FILE } from '../actions/addMdActions';
 
-export default function reducer(state = '', action) {
+const initialState = {
+  formInput: '',
+  mdArray: [{ title: 'Title', body: 'Hello' }]
+};
+
+export default function reducer(state = initialState, action) {
   switch(action.type) {
     case UPDATE_FORM_INPUT:
-      return action.payload;
+      return { ...state, formInput: action.payload };
+    case ADD_MD_FILE:
+      return { ...state, mdArray: [...state.mdArray, action.payload] };
     default:
       return state;
   }
--- src/reducers/AddMdReducer.test.js
+++ src/reducers/AddMdReducer.test.js
@@ -1,9 +1,11 @@
 import addMdReducer from './AddMdReducer';
-import { updateFormInput } from '../actions/addMdActions';
+import { updateFormInput, addMdFile } from '../actions/addMdActions';
 
 describe('addMdReducer tests', () => {
   it('returns default state', () => {
-    const state = 'Hello';
+    const state = {
+      formInput: 'Hello'
+    };
 
     const action = {
       type: 'Nothing',
@@ -15,11 +17,24 @@ describe('addMdReducer tests', () => {
   });
 
   it('updates state', () => {
-    const state = 'Hello';
+    const state = {
+      formInput: 'Hello'
+    };
 
     const action = updateFormInput('Hell');
 
     const newState = addMdReducer(state, action);
-    expect(newState).toEqual('Hell');
+    expect(newState).toEqual({ formInput: 'Hell' });
+  });
+
+  it('adding an MD file object to array in state', () => {
+    const state = {
+      mdArray: [{ title: 'Title', body: 'Hello' }]
+    };
+
+    const action = addMdFile('Hello');
+
+    const newState = addMdReducer(state, action);
+    expect(newState).toEqual({ mdArray: [{ title: 'Title', body: 'Hello' }, { title: 'Hello', body: '' }] });
   });
 });
