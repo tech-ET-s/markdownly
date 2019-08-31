c9eacb0fe322e0030931e23e4197f614d01fb4e2
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-30 18:28:36 -0700

    Fixed the file name there was an extra space

--- src/containers/MarkdownEditor.js
+++ src/containers/MarkdownEditor.js
@@ -3,13 +3,13 @@ import Document from '../components/markdown/Document';
 import { getBody } from '../selectors/markdownSelectors';
 import { updateMdBody } from '../actions/markdownActions';
 
-const mapStateToProps = (state, index = 0) => ({
-  markdown: getBody(state, index)
+const mapStateToProps = state => ({
+  markdown: getBody(state)
 });
 
 const mapDispatchToProps = dispatch => ({
   updateMarkdown({ target }) {
-    dispatch(updateMdBody(target.value, 0));
+    dispatch(updateMdBody(target.value));
   }
 });
 
