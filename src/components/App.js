280cde32e7bf81f4b39b039f0d1621f86da4265a
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-30 18:29:20 -0700

    SaveMdFiles and MarkdownNav appended to app for visual

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MarkdownEditor from '../containers/MarkdownEditor';
+import MarkdownNav from '../containers/MarkdownNav';
+import SaveMdFiles from '../containers/SaveMdFiles';
 
 export default function App() {
   return (
     <>
       <h1>Markdown Editor</h1>
       <MarkdownEditor />
+      <SaveMdFiles />
+      <MarkdownNav />
     </>
   );
 }
